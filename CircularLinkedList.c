d8e66ad24533c910b4f1ef0a3504ff6ff94c113a
Anika Jairath <ajairath2000@gmail.com>
2025-09-04 12:06:26 +0530

    Add files via upload

---
+++ CircularLinkedList.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+
+int data;
+struct node * link;
+
+};
+
+void main(){
+
+struct node *head; //TO POINT TO ADDRESS OF STARTING NODE
+struct node *newnode; //TO POINT TO ADDRESS OF NEW NODE
+struct node *temp; //TO POINT TO ADDRESS OF LAST NODE
+head = NULL;
+int choice;
+
+printf("\t\t\t\t\t       CIRCULAR LINKED LIST\n");
+
+do{
+newnode = (struct node*) malloc(sizeof(struct node));
+
+printf("\nENTER DATA:\n");
+scanf("%d", &newnode->data); //STORING DATA IN 'DATA' PART OF NEW NODE
+newnode->link = head; //SETTING THE LINK PART TO 0
+
+if(head == NULL){ //FIRST NODE
+    head = newnode;
+    temp = newnode;
+    newnode->link = head;
+}
+
+else{
+    temp->link = newnode; //STORE ADDRESS OF NEW NODE IN OLD NODE (POINTED BY TEMP AT THE MOMENT) LINK PART
+    temp = newnode; //UPDATE TEMP TO POINT TO NEW NODE NOW
+}
+
+printf("\nDO YOU WANT TO ENTER A NEW NODE?\nENTER 0 FOR YES AND 1 FOR NO\n");
+scanf("%d", &choice);
+
+}while(choice == 0);
+
+
+struct node *curr = head;
+
+printf("\nNOW PRINTING THE LIST....\n");
+
+while(curr->link != head){
+
+    printf("\n%d", curr->data);
+    curr = curr->link;
+}
+printf("\n%d", curr->data);
+
+printf("\n\n");
+
+}
+
